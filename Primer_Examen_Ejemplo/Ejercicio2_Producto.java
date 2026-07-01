56561226171cf4e659d527f218f7fb12c308b75d
juanperoh11-lgtm <juanperoh11@gmail.com>
2026-07-01 09:40:13 +0200

    Añadido Ejercicio 2 de productos en Java

---
+++ Primer_Examen_Ejemplo/Ejercicio2_Producto.java
@@ -0,0 +1,13 @@
+public class Ejercicio2_Producto {
+    String nombre;
+    double precio;
+
+	public Ejercicio2_Producto(String nombre, double precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public  void Mostrar(){
+		System.out.println("Nombre del producto: " + nombre + " Precio del producto " + precio);
+	}
+}
